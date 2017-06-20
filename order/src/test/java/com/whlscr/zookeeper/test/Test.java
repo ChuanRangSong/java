08f40145db4aa2c448c217a0d9f17086164d8c3d
administrator <administrator@localhost>
2017-06-20 17:03:47 +0800

    增加package层级

--- order/src/test/java/com/whlscr/zookeeper/test/Test.java
+++ order/src/test/java/com/whlscr/zookeeper/test/Test.java
@@ -1,4 +1,4 @@
-package com.whlscr.zookeeper;
+package com.whlscr.zookeeper.test;
 
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.WatchedEvent;
@@ -9,17 +9,17 @@ import org.apache.zookeeper.ZooKeeper;
 public class Test {
 	@org.junit.Test
 	public void fun() throws Exception{
-		String host = "47.92.34.189";
-		int port = 2181;
+		String connectString = "47.92.34.189:2181,47.92.34.189:2182,47.92.34.189:2183";
 		int connectionTimeOut = 10000;
 		 // 创建一个与服务器的连接
-		 final ZooKeeper zk = new ZooKeeper(host + ":" + port, connectionTimeOut, 
+		 final ZooKeeper zk = new ZooKeeper(connectString, connectionTimeOut, 
 				 new Watcher() { 
 		            // 监控所有被触发的事件
 		            public void process(WatchedEvent event) { 
 		                System.out.println(event.getPath() +"路径已经触发了" + event.getType() + "事件！");
 		            }
 		        }); 
+		 
 		 // 创建一个目录节点
 		 zk.create("/testRootPath", "testRootData".getBytes(), Ids.OPEN_ACL_UNSAFE,
 		   CreateMode.PERSISTENT); 
@@ -42,6 +42,36 @@ public class Test {
 		 // 删除父目录节点
 		 zk.delete("/testRootPath",-1); 
 		 // 关闭连接
+		 
 		 zk.close();
+	}
+	@org.junit.Test
+	public void fun1(){
+		String fileName = "ljd$fla$jd$fl$";
+		
+		StringBuffer buffer = new StringBuffer();
+		StringBuffer key = new StringBuffer();
+		boolean began = false;
+		boolean end = false;
+		
+		for (int i = 0; i < fileName.length(); i++) {
+			char c = fileName.charAt(i);
+			if (c == '$') {
+				if(!began){
+					began = true;
+					end = false;
+				} else {
+					began = false;
+					end = true;
+					buffer.append("++" + key.toString() + "++");
+					key.setLength(0);
+				}
+			}else if (began && !end) {
+				key.append(c);
+			} else {
+				buffer.append(c);
+			}
+		}
+		System.out.println(buffer.toString());
 	}
 }
