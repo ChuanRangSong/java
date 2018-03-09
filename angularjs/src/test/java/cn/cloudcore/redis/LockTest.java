9c770bd2c2effecf6e3efc87010eb6ba46b76622
ChuanRangSong <scr0419@outlook.com>
2018-03-09 17:09:05 +0800

    新增测试

--- angularjs/src/test/java/cn/cloudcore/redis/LockTest.java
+++ angularjs/src/test/java/cn/cloudcore/redis/LockTest.java
@@ -1,8 +1,157 @@
 package cn.cloudcore.redis;
 
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.Transaction;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
 public class LockTest {
 
-    public static void main(String[] args) {
+    private static JedisPool pool = null;
 
+    static {
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxTotal(200);
+        config.setMaxIdle(50);
+        config.setMinIdle(8);//设置最小空闲数
+        config.setMaxWaitMillis(10000);
+        config.setTestOnBorrow(true);
+        config.setTestOnReturn(true);
+//Idle时进行连接扫描
+        config.setTestWhileIdle(true);
+//表示idle object evitor两次扫描之间要sleep的毫秒数
+        config.setTimeBetweenEvictionRunsMillis(30000);
+//表示idle object evitor每次扫描的最多的对象数
+        config.setNumTestsPerEvictionRun(10);
+//表示一个对象至少停留在idle状态的最短时间，然后才能被idle object evitor扫描并驱逐；这一项只有在timeBetweenEvictionRunsMillis大于0时才有意义
+        config.setMinEvictableIdleTimeMillis(60000);
+        pool = new JedisPool(config, "47.92.34.189", 6379, 30000);
+    }
+
+    private static Lock lock = new Lock(pool);
+
+    private static int n = 100;
+
+    public static void seckill(){
+        String identifier = lock.lock("resource", 5000, 10000);
+        if (null != identifier) {
+            System.out.println(Thread.currentThread().getName() + "-----获得锁----" + identifier);
+            int num = n;
+            try {
+                Thread.sleep(100);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println(n);
+            n = num - 1;
+            lock.unLock("resource", identifier);
+        } else {
+            //System.out.println(Thread.currentThread().getName() + "--超时");
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 200; i++) {
+            new Thread(() -> {
+                while (n > 0) {
+                    seckill();
+                }
+            }).start();
+        }
+    }
+}
+
+class Lock {
+
+    private JedisPool jedisPool;
+
+    public Lock(JedisPool jedisPool) {
+        this.jedisPool = jedisPool;
+    }
+
+    public String lock(String lockName, long acquireTimeOut, long timeOut) {
+
+        Jedis jedis = null;
+
+        try {
+            jedis = jedisPool.getResource();
+
+
+            String identifier = UUID.randomUUID().toString();
+
+            long timeEnd = System.currentTimeMillis() + acquireTimeOut;
+            while (System.currentTimeMillis() < timeEnd) {
+
+                if ("OK".equals(jedis.set(lockName, identifier, "NX", "PX", timeOut))) {
+                    return identifier;
+                }
+                /*if (jedis.setnx(lockName, identifier) == 1) {
+                    jedis.expire(lockName, lockExpire);
+                    return identifier;
+                }
+
+                if (jedis.ttl(lockName) == -1) {
+                    jedis.expire(lockName, lockExpire);
+                }*/
+//                System.out.println("过程。。。");
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (null != jedis) {
+                jedis.close();
+            }
+        }
+        return null;
+    }
+
+    public void unLock(String lockName, String identifier) {
+        Jedis jedis = null;
+        try {
+            jedis = jedisPool.getResource();
+
+            String script = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
+            Object result = jedis.eval(script, Collections.singletonList(lockName), Collections.singletonList(identifier));
+            if (new Long(1).equals(result)) {
+                System.out.println(Thread.currentThread().getName() + "释放锁++++" + identifier);
+            } else {
+                System.out.println(Thread.currentThread().getName() + "释放锁【【【【" + identifier);
+            }
+
+            /*if (identifier.equals(jedis.get(lockName))) {
+                jedis.del(lockName);
+            }*/
+//            while (true) {
+//                jedis.watch(lockName);
+//                if (identifier.equals(jedis.get(lockName))) {
+//                    Transaction transaction = jedis.multi();
+//                    transaction.del(lockName);
+//                    List<Object> results = transaction.exec();
+//                    if (results == null) {
+//                        continue;
+//                    }
+//                }
+//                jedis.unwatch();
+//                break;
+//            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (null != jedis) {
+                jedis.close();
+            }
+        }
     }
 }
--- angularjs/src/test/java/cn/cloudcore/zookeeper/LockTest.java
+++ angularjs/src/test/java/cn/cloudcore/zookeeper/LockTest.java
@@ -1,38 +1,159 @@
 package cn.cloudcore.zookeeper;
 
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 public class LockTest {
 
+    private static ZooKeeper zooKeeper;
+
+    private static String connectString = "47.92.34.189:2181,47.92.34.189:2182,47.92.34.189:2183";
+
+    private static int sessionTimeout = 100;
+
+    private static String rootPath = "/locks";
+
+    private static int num = 100;
+
     public static void main(String[] args) {
-        Lock lock = new ReentrantLock();    //注意这个地方
+//        initZooKeeper();
+//
+//        createLock();
+//
+//        doSomething();
 
-        new Thread(() -> insert(Thread.currentThread(), lock)).start();
-        new Thread(() -> insert(Thread.currentThread(), lock)).start();
+        //closeZooKeeper();
 
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException exception) {
-            exception.printStackTrace();
+        test();
+    }
+
+    public static void doSomething() {
+        for (int i = 0; i < 20; i++) {
+            new Thread(() -> {
+                String lock = lock();
+                int a = num;
+                try {
+                    Thread.sleep(100);
+                    System.out.println(a);
+                    num = a - 1;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                unLock(lock);
+            }).start();
         }
     }
 
-    public static void insert(Thread thread, Lock lock) {
+    public static String lock() {
+        try {
+            String value = zooKeeper.create(rootPath + "/lock", "a".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+            List<String> locks = zooKeeper.getChildren(rootPath, false);
+            Collections.sort(locks);
+            int index = locks.indexOf(value.substring(rootPath.length() + 1));
+            if (index != 0) {
+                CountDownLatch latch = new CountDownLatch(1);
 
-        if(lock.tryLock()){
-            try {
-                System.out.println(thread.getName() + "得到了锁");
-                thread.sleep(1000);
-            } catch (Exception e) {
-                // TODO: handle exception
-            }finally {
-                System.out.println(thread.getName() + "释放了锁");
-                lock.unlock();
+                Stat stat = zooKeeper.exists(rootPath + "/" + locks.get(index - 1), event -> latch.countDown());
+
+                if (stat != null ) {
+                    latch.await();
+                }
             }
-        } else {
-            System.out.println(thread.getName() + "获取锁失败！");
+            return value;
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        return null;
+    }
+
+    public static void unLock(String lock) {
+        try {
+            zooKeeper.delete(lock, -1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void createLock() {
+        try {
+            Stat stat = zooKeeper.exists(rootPath, false);
+            if (null == stat) {
+                zooKeeper.create(rootPath, new byte[0], ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void initZooKeeper() {
+        try {
+            CountDownLatch latch = new CountDownLatch(1);
+            zooKeeper = new ZooKeeper(connectString, sessionTimeout, event -> {
+                System.out.println("获取连接");
+                latch.countDown();
+            });
+
+            latch.await();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void closeZooKeeper() {
+        try {
+           zooKeeper.close();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void test() {
+        //创建zookeeper的客户端
+        try {
+
+
+            for (int i = 0; i < 20; i++) {
+                new Thread(() -> {
+
+                    RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+                    CuratorFramework client = null;
+                    try {
+                        client = CuratorFrameworkFactory.newClient("47.92.34.189:2181,47.92.34.189:2182,47.92.34.189:2183", retryPolicy);
+                        client.start();
+                        //创建分布式锁, 锁空间的根节点路径为/curator/lock
+                        InterProcessMutex mutex = new InterProcessMutex(client, "/curator/lock");
+                        mutex.acquire();
+                        int a = num;
+                        Thread.sleep(100);
+                        System.out.println(a);
+                        num = a - 1;
+                        mutex.release();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    } finally {
+                        if (null != client) {
+                            client.close();
+                        }
+                    }
+
+                }).start();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //关闭客户端
     }
 
 }
