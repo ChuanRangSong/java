b63a9f43c7043a01e4004be07578727e309e982a
ChuanRangSong <scr0419@outlook.com>
2018-05-06 02:07:26 +0800

    增加连接Redis集群测试

--- order/src/main/java/com/baidu/order/Test.java
+++ order/src/main/java/com/baidu/order/Test.java
@@ -1,13 +1,45 @@
 package com.baidu.order;
 
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisCluster;
+
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 测试类
  * Created by song on 2017/3/2.
  */
 public class Test {
+
+    private static JedisCluster jedisCluster=null;
+    private static Set<HostAndPort> hostAndPorts=null;
+
     @org.junit.Test
     public void fun(){
-        Singleton singleton = Singleton.getTnstence();
-        singleton.show();
+        jedisCluster = getJedisCluster();
+        System.out.println(jedisCluster.get("name"));
+    }
+
+    public static  Set<HostAndPort> getHostAndPort(String hostAndPort){
+        Set<HostAndPort> hap = new HashSet();
+        String[] hosts = hostAndPort.split(",");
+        String[] hs;
+        for(String host:hosts){
+            hs=host.split(":");
+            hap.add(new HostAndPort(hs[0], Integer.parseInt(hs[1])));
+        }
+        return hap;
+    }
+
+    public static JedisCluster getJedisCluster(){
+        GenericObjectPoolConfig gopc = new GenericObjectPoolConfig();
+        gopc.setMaxTotal(32);
+        gopc.setMaxIdle(4);
+        gopc.setMaxWaitMillis(6000);
+        hostAndPorts = getHostAndPort("47.52.97.135:9001");
+        jedisCluster = new JedisCluster(hostAndPorts, 2000, 2000, 3,null,gopc);
+        return jedisCluster;
     }
 }
