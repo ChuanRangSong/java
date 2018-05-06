56f48ed7847ca17416022b50febca8129622bb76
ChuanRangSong <scr0419@outlook.com>
2018-05-06 15:22:59 +0800

    增加restful风格

--- spring-boot-starter/src/main/java/com/baidu/springbootstarter/controller/HelloWorldController.java
+++ spring-boot-starter/src/main/java/com/baidu/springbootstarter/controller/HelloWorldController.java
@@ -1,14 +1,21 @@
 package com.baidu.springbootstarter.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.baidu.springbootstarter.model.User;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
+@RequestMapping("/hello")
 public class HelloWorldController {
 
-    @RequestMapping("/hello")
-    public String index() {
-        return "Hello !";
+    @RequestMapping(value = "/index/{id}", method = RequestMethod.GET)
+    public String index(@PathVariable String id, @ModelAttribute User user, @RequestParam String pwd) {
+        System.out.println(user.getId());
+        System.out.println(pwd);
+
+        return id;
     }
 
     @RequestMapping("/helloworld")
